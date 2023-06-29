32234dd6bc7d5a9ca12e07cee3eff9017d6a5810
hiitgmanshu <himanshushekhar44578@gmail.com>
2023-06-30 01:59:42 +0530

    Social connection

--- form.js
+++ form.js
@@ -7,3 +7,23 @@ document.querySelectorAll(".active").forEach(function(element) {
         document.querySelector(".one").classList.remove("dropdown_menu");
     });
 });
+
+function openProfile(profile) {
+    switch (profile) {
+        case 'instagram':
+            window.open('https://www.instagram.com/h_shekhar_302', '_blank');
+            break;
+        case 'github':
+            window.open('https://github.com/hiitgmanshu', '_blank');
+            break;
+        case 'twitter':
+            window.open('https://twitter.com/Himansh52472191', '_blank');
+            break;
+        default:
+            break;
+    }
+}
+
+function sendEmail() {
+    window.open('mailto:himanshushekhar44578@gmail.com');
+}
